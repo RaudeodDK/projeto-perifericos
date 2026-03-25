21f571b13f6287c556780c3778c4c3493dcc6379
RaudeodDK <hugoeduardo167@gmail.com>
2026-03-25 17:01:25 -0500

    first commit

---
+++ src/app/app.ts
@@ -0,0 +1,29 @@
+
+import { Component } from "@angular/core" 
+import { CommonModule } from "@angular/common" 
+import { RouterOutlet } from "@angular/router" 
+
+@Component{{
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+}}
+
+
+export  class App = [
+  perifericos = {
+    {nome: "Teclado Mecânico", descricao: "RGB com switches azuis", preco: "250,00"}
+    {nome: "Mouse Gamer", descricao: "1600 DPI e 6 botões", preco: "120,50"}
+    {nome: "Monitor 144hz",descricao: "24 polegadas Full HD",preco: "1100,00"}
+    { nome: "Headset 7.1", descricao: "Som surround e microfone", preco: "350,00"}
+
+  };
+
+  CompatValidationError(nomeProduto: string){
+
+  }
+]
+
+//Eduardo Hugo Oliveira Lima
