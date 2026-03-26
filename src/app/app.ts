0d808cdcddf18060d93f10d3ebb1ef553448aed7
RaudeodDK <hugoeduardo167@gmail.com>
2026-03-26 14:23:27 -0500

    finalizando

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,29 +1,30 @@
 
-import { Component } from "@angular/core" 
-import { CommonModule } from "@angular/common" 
-import { RouterOutlet } from "@angular/router" 
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
 
-@Component{{
+@Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
-}}
+  styleUrls: ['./app.css']
+})
 
 
-export  class App = [
-  perifericos = {
-    {nome: "Teclado Mecânico", descricao: "RGB com switches azuis", preco: "250,00"}
-    {nome: "Mouse Gamer", descricao: "1600 DPI e 6 botões", preco: "120,50"}
-    {nome: "Monitor 144hz",descricao: "24 polegadas Full HD",preco: "1100,00"}
-    { nome: "Headset 7.1", descricao: "Som surround e microfone", preco: "350,00"}
+export  class App  {
+  perifericos = [
+  
+    {nome: "Teclado Mecânico", descricao: "RGB com switches azuis", preco: 250.00},
+    {nome: "Mouse Gamer", descricao: "1600 DPI e 6 botões", preco: 120.50},
+    {nome: "Monitor 144hz",descricao: "24 polegadas Full HD",preco: 1100.00},
+    {nome: "Headset 7.1", descricao: "Som surround e microfone", preco: 350.00}
 
-  };
+  ];
 
-  CompatValidationError(nomeProduto: string){
-
+  comprar(nomeProduto: string){
+    alert("Comprou: " + nomeProduto);
   }
-]
+}
 
 //Eduardo Hugo Oliveira Lima
