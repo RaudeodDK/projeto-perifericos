0ff9f15f0aed0468e7c3fb00b9b6b8ee4d48f603
RaudeodDK <hugoeduardo167@gmail.com>
2026-03-31 16:45:36 -0500

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,30 +1,21 @@
 
-import { Component } from "@angular/core";
-import { CurrencyPipe } from "@angular/common";
+import { Component, signal } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { Menu } from "./menu/menu";
+import { Tourz } from "./tourz/tourz";
+import { Footer} from "./footer/footer";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CurrencyPipe, RouterOutlet],
+  imports: [RouterOutlet, Menu, Tourz, Footer],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
 
 
-export  class App  {
-  perifericos = [
-  
-    {nome: "Teclado Mecânico", descricao: "RGB com switches azuis", preco: 250.00},
-    {nome: "Mouse Gamer", descricao: "1600 DPI e 6 botões", preco: 120.50},
-    {nome: "Monitor 144hz",descricao: "24 polegadas Full HD",preco: 1100.00},
-    {nome: "Headset 7.1", descricao: "Som surround e microfone", preco: 350.00}
-
-  ];
-
-  comprar(nomeProduto: string){
-    alert("Comprou: " + nomeProduto);
-  }
+export class App {
+  protected readonly title = signal('projeto-perifericos-angular');
 }
 
 //Eduardo Hugo Oliveira Lima
